d9ba1152bbce2d0b53be1ea0cb870ca6eb5b7ff9
Jesus Pujada <jpujada30@gmail.com>
2024-04-25 14:25:30 +0200

    Fase2

--- FASE2/PUJADA_JESUS_fase2.sql
+++ FASE2/PUJADA_JESUS_fase2.sql
@@ -38,3 +38,59 @@ Requeriment 1 (1.5 punts):
 -- li ha assignat el codi <<codi>>”
 
 
+CREATE OR REPLACE PROCEDURE NouJugador(
+    p_nom jugador.NOMBRE%type,
+    p_procedencia jugador.PROCEDENCIA%type,
+    p_alçada jugador.ALTURA%TYPE,
+    p_pes jugador.PESO%type,
+    p_posicio jugador.NOMBRE_EQUIPO%type
+)
+
+IS
+
+BEGIN
+    insert INTO JUGADOR VALUES(CODIGO,)
+END;
+/
+
+
+
+
+
+
+
+--                                   REVISAR
+SQL> DESC JUGADOR;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ CODIGO                                    NOT NULL NUMBER(38)
+ NOMBRE                                             VARCHAR2(30)
+ PROCEDENCIA                                        VARCHAR2(20)
+ ALTURA                                             VARCHAR2(4)
+ PESO                                               NUMBER(38)
+ POSICION                                           VARCHAR2(5)
+ NOMBRE_EQUIPO                                      VARCHAR2(20)
+
+SQL> insert into jugador (codigo, peso) values (10000,678);
+insert into jugador (codigo, peso) values (10000,678)
+*
+ERROR at line 1:
+ORA-02290: check constraint (SYSTEM.CH_PESO) violated
+
+-- Hemos intentado hacer un insert con el peso 678 pero si hacemos un 
+select constraint_name, search_condition from user_constraints where table_name='JUGADOR';
+
+-- Nos aparece lo siguiente
+
+CONSTRAINT_NAME
+------------------------------
+SEARCH_CONDITION
+--------------------------------------------------------------------------------
+CH_PESO
+peso>=130 AND peso<=400
+
+Al hacer el insert nos aparece:
+ERROR at line 1:
+ORA-02290: check constraint (SYSTEM.CH_PESO) violated
+
+Tendriamos que lanzar una excepcion con el siguiente codigo: 2290 
